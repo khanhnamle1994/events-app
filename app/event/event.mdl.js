33f095ce07f2128228a087a6332752aaaeed3f73
James Le <le_j6@denison.edu>
2017-07-26 20:50:45 -0700

    Setup Event Module

---
+++ app/event/event.mdl.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('event', [
+            'event.profile',
+            'event.feed',
+            'event.add'
+        ])
+        .config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function config($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('main.event', {
+                url: 'events',
+                views: {
+                    '': {
+                        templateUrl: '../views/event/events.html',
+                        controller: 'EventCtrl as vm'
+                    }
+                },
+                data: {
+                    is_granted: ['ROLE_USER']
+                }
+            });
+    }
+
+})();
